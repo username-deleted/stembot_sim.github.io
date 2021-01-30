06b76df7d464b5dc8fc024e1d4ad980219337bfe
Mason Fleming <mgfleming@unomaha.edu>
2021-01-29 18:10:51 -0600

    create SIMbot script, begin consolidation of attachment functionality onto SIMbot prefab

--- Assets/Scripts/SC_MainMenu.cs
+++ Assets/Scripts/SC_MainMenu.cs
@@ -9,6 +9,8 @@ public class SC_MainMenu : MonoBehaviour
     public GameObject MainMenu;
     public GameObject OptionsMenu;
     public GameObject CustomizationMenu;
+
+    public GameObject SIMBot;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/SIMbot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SIMbot : MonoBehaviour
+{
+    public GameObject[] attachments;
+    public int attachmentNumber = 0;
+    private int MAX_ATTACHMENT_INDEX;
+
+
+    private void Start()
+    {
+        MAX_ATTACHMENT_INDEX = attachments.Length - 1;
+    }
+
+    public void NextAttachment()
+    {
+        if(attachmentNumber == MAX_ATTACHMENT_INDEX)
+        {
+            attachmentNumber = 0;
+        }
+        else
+        {
+            attachmentNumber++;
+        }
+    }
+
+    public void PreviousAttachment()
+    {
+        if(attachmentNumber == 0)
+        {
+            attachmentNumber = MAX_ATTACHMENT_INDEX;
+        }
+        else
+        {
+            attachmentNumber--;
+        }
+    }
+}
