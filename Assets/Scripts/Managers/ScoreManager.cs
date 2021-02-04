8379ed99f57842bd6a3f024dc5d0f7c21397edee
Cassie Miller <cbmiller@unomaha.edu>
2021-02-04 13:56:21 -0600

    Score and timer completed and added

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -5,28 +5,23 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int score = 0;
+    private int currentScore = 0;
     private int maxScore;
+    private Text scoreDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
         //The max score is based on how many coins are in the scene.
         maxScore = GameObject.FindGameObjectsWithTag("Coin").Length;
+        scoreDisplay = GameObject.FindGameObjectWithTag("ScoreValue").GetComponent<Text>();
+        scoreDisplay.text = currentScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    /*AddToScore(int)
-    Desc: Increments the score variable by the value passed in.
-    Parameters: int, point to be added to score
-    Return: None*/
+    //increment the score by the value passed in.
     public void addScore(int point)
     {
-        score += point;
-        Debug.Log(score);
+        currentScore += point;
+        scoreDisplay.text = currentScore.ToString();
     }
 }
