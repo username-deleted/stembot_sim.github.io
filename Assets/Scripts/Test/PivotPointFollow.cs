9477c904e491897bef34bbe26020a2ed5edd95db
Cassie Miller <cbmiller@unomaha.edu>
2021-03-02 16:10:50 -0600

    testing

--- Assets/Scripts/Test/PivotPointFollow.cs
+++ Assets/Scripts/Test/PivotPointFollow.cs
@@ -12,9 +12,13 @@ public class PivotPointFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float nani;
         //gameObject refers to the object the script is on. In this case, the pivot point of the camera on the SIMbot.
         gameObject.transform.position = new Vector3(SIMbot.transform.position.x, SIMbot.transform.position.y, SIMbot.transform.position.z);
         //Euler angles must be used when trying to set the rotation of one object to the rotation of another. Setting specific quaternion values to another quaternion value can lead to odd behaviors.
+        Debug.Log("Angle 1: " + SIMbot.transform.rotation.eulerAngles.y);
+        nani = SIMbot.transform.rotation.eulerAngles.y;
+        Debug.Log("Angle 2: " + nani);
         gameObject.transform.rotation = Quaternion.Euler(new Vector3(gameObject.transform.rotation.eulerAngles.x, SIMbot.transform.rotation.eulerAngles.y, gameObject.transform.rotation.eulerAngles.z));
     }
 }
