b722023262247895d33ba5b1e17bb7ac85adfb56
Mason Fleming <mgfleming@unomaha.edu>
2021-03-02 16:02:51 -0600

    fix typo in orbit script, clean up code, make start/update methods consistent in comse scripts

--- Assets/Scripts/Test/Python/PythonBot.cs
+++ Assets/Scripts/Test/Python/PythonBot.cs
@@ -8,7 +8,7 @@ public class PythonBot : MonoBehaviour
     public GameObject motors;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         var engine = global::UnityPython.CreateEngine();
 
