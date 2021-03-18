85fce12d09af7e3a4ca7b7702910166715dfd6fe
Mason Fleming <mgfleming@unomaha.edu>
2021-03-18 06:18:43 -0500

    docs(Core): LevelManager.cs comments

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,11 +4,21 @@ using UnityEngine.InputSystem;
 
 public class LevelManager : MonoBehaviour
 {
+    /// <summary>Instance variable <c>selectedLevel</c> represents the
+    /// currently selected level.</summary>
+    public int selectedLevel = 1;
 
-    public int selectedLevel = 1; //the currently selected level
-    public string[] sceneNames; //the list of scene names
-    public bool isPaused; //whether the game is paused or not
-    private readonly string MAIN_MENU_SCENE_NAME = "FinalMainMenu"; //the scene name of our main menu scene
+    /// <summary>Instance variable <c>sceneNames</c> represents the
+    /// list of scene names.</summary>
+    public string[] sceneNames;
+
+    /// <summary>Instance variable <c>isPaused</c> whether the
+    /// game is paused or not.</summary>
+    public bool isPaused;
+
+    /// <summary>Instance variable <c>MAIN_MENU_SCENE_NAME</c> the
+    /// scene name of our main menu scene.</summary>
+    private readonly string MAIN_MENU_SCENE_NAME = "FinalMainMenu";
 
     private void Start()
     {
@@ -23,25 +33,30 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-
+    /// <summary>This method changes the point's location to
+    /// the given coordinates.</summary>
+    /// <param><c>levelNumber</c> is the new level number.</param>
     public void SetSelectedLevel(int levelNumber)
     {
         selectedLevel = levelNumber;
     }
 
+    /// <summary>This method returns the selected level.</summary>
+    /// <returns>The selected level</returns>
     public int GetSelectedLevel()
     {
         return selectedLevel;
     }
 
-    //load a level with the passed in level number
+    /// <summary>This method sets the selected level to the given level number.</summary>
+    /// <param><c>levelNumber</c> is the new level number.</param>
     public void LoadLevel(int levelNumber)
     {
         SetSelectedLevel(levelNumber);
         LoadLevel();
     }
 
-    //load a scene with the current level number
+    /// <summary>Load a scene with the current level number.</summary>
     public void LoadLevel()
     {
         //a scene number cannot be less than 0, or higher than the amount of levels we have
@@ -59,8 +74,7 @@ public class LevelManager : MonoBehaviour
         Time.timeScale = 1;
     }
 
-
-    //pause the level
+    /// <summary>Pauses the level.</summary>
     public void PauseLevel()
     {
         isPaused = true;
@@ -68,7 +82,7 @@ public class LevelManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
     }
 
-    //resume the level
+    /// <summary>Resumes the level.</summary>
     public void ResumeLevel()
     {
         isPaused = false;
@@ -76,22 +90,26 @@ public class LevelManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    //return the current scene's level name
-    public string GetLevelName()
-    {
-        return SceneManager.GetActiveScene().name;
-    }
-
-    //return whether or not we are in the main menu scene
-    public bool InMainMenuScene()
-    {
-        return SceneManager.GetActiveScene().name == MAIN_MENU_SCENE_NAME;
-    }
-
+    /// <summary>Restarts the level.</summary>
     public void RestartLevel()
     {
         string nameOfCurrentScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(nameOfCurrentScene);
         Time.timeScale = 1;
     }
+
+    /// <summary>Return the current scene's name</summary>
+    /// <returns>The scene's name.</returns>
+    public string GetLevelName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    /// <summary>Returns whether or not we are in the main menu scene.</summary>
+    /// <returns>True if we're in the main menu scene; otherwise, false.</returns>
+    public bool InMainMenuScene()
+    {
+        return SceneManager.GetActiveScene().name == MAIN_MENU_SCENE_NAME;
+    }
+
 }
