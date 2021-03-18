6505773c64f4e553b23aca478ef75169d15321cd
Mason Fleming <mgfleming@unomaha.edu>
2021-03-18 06:24:24 -0500

    docs(Core): SaveManager.cs comments

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
+    /// <summary>Instance variable <c>SIMbot</c> represents the
+    /// SIMbot <c>GameObject<c> in the scene.</summary>
     private GameObject SIMbot;
+
+    /// <summary>Instance variable <c>SIMbotScript</c> represents the
+    /// SIMbot script <c>SIMbot<c> in the scene.</summary>
     private SIMbot SIMbotScript;
 
     private void Start()
@@ -13,18 +18,20 @@ public class SaveManager : MonoBehaviour
         SIMbotScript = SIMbot.GetComponent<SIMbot>();
     }
 
-    //save the data on the simbot to a file
+    /// <summary>Save the data on the SIMbot to a file.</summary>
     public void SaveSBData()
     {
         string saveString = JsonUtility.ToJson(SIMbotScript.SBData);
         SaveSystem.Save(saveString);
     }
 
+    /// <summary>Load the SIMbot data from a file.</summary>
     public string LoadSBData()
     {
         return SaveSystem.Load();
     }
 
+    /// <summary>Delete data on quit.</summary>
     private void OnApplicationQuit()
     {
         //we are not doing a persistent save system, delete the data so the app is fresh on start
