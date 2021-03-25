47a96d0b73628c044ef6f163d3ce5f37870e52e7
Mason Fleming <mgfleming@unomaha.edu>
2021-03-25 04:16:53 -0500

    feat(Core) main menu selection sounds added

--- Assets/Scripts/Menu/SelectableButton.cs
+++ Assets/Scripts/Menu/SelectableButton.cs
@@ -5,6 +5,11 @@ using UnityEngine.EventSystems;
 
 public class SelectableButton : MonoBehaviour, ISelectHandler
 {
+    public void OnSelect(BaseEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
