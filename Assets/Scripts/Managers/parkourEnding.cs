48fc93f527f354a6c06aa6f749b4f5b0400c7250
jhenery <54946731+jhenery@users.noreply.github.com>
2021-04-12 18:05:50 -0500

    Working out an ending to the parkour level
    added a script to end the level when the final area comes into contact with the bot

---
+++ Assets/Scripts/Managers/parkourEnding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class parkourEnding : MonoBehaviour
+{
+    private SC_CompletionMenu completionMenu;
+    private TimeManager timeManager;
+    private Text completionMenuTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        completionMenu = GameObject.FindGameObjectWithTag("CompletionCanvas").GetComponent<SC_CompletionMenu>();
+        timeManager = GameObject.FindGameObjectWithTag("Managers").GetComponent<TimeManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "SIMbot 3-5-21")
+        {
+            completionMenu.enableCompletionMenu();
+            completionMenuTimeText.text = timeManager.getTime().ToString("F2");
+        }
+    }
+}
